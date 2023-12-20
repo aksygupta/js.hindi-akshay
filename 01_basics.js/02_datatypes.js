c021e7ac913e1c80f771ec9b7046c733ebd7d7f3
aksygupta <130675388+aksygupta@users.noreply.github.com>
2023-12-20 18:41:42 +0000

    datatypes

---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as new version
+//alert("hello")
+console.log(3+3);
+let name = "akshay"
+let age = 21
+let isLoggedIn = false
+//number =>2 to power 53
+//bifint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+console.log(typeof null);
